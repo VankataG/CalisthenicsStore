3b7b5870986bc15caf61a90ccd968c3c533dec10
Ivan Ganchev <djonatan2002@gmail.com>
2025-06-04 06:55:50 +0300

    Added input form model for Create Product operation

---
+++ CalisthenicsStore.ViewModels/Product/AddProductInputModel.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CalisthenicsStore.ViewModels.Product
+{
+    public class AddProductInputModel
+    {
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; } = new List<SelectListItem>();
+    }
+}
