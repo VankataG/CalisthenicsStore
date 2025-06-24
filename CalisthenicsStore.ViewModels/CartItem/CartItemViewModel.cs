84311abc8329970cf34e86dcde305e9e48f9d9d3
Ivan Ganchev <djonatan2002@gmail.com>
2025-06-24 19:04:20 +0300

    Added CartItem ViewModel that we might NOT need

---
+++ CalisthenicsStore.ViewModels/CartItem/CartItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CalisthenicsStore.ViewModels.CartItem
+{
+    public class CartItemViewModel
+    {
+        public string ProductName { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
