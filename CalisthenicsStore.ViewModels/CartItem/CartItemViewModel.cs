151e8497ed6d8714c9723fbcd6603c1db871fe34
Ivan Ganchev <djonatan2002@gmail.com>
2025-06-25 08:03:18 +0300

    changed ViewModels it was important and the Views with them

--- CalisthenicsStore.ViewModels/CartItem/CartItemViewModel.cs
+++ CalisthenicsStore.ViewModels/CartItem/CartItemViewModel.cs
@@ -2,7 +2,10 @@
 {
     public class CartItemViewModel
     {
+        public int ProductId { get; set; }
         public string ProductName { get; set; }
+
+        public string ImageUrl { get; set; }   
 
         public int Quantity { get; set; }
 
--- CalisthenicsStore.ViewModels/Order/CheckoutViewModel.cs
+++ CalisthenicsStore.ViewModels/Order/CheckoutViewModel.cs
@@ -4,7 +4,7 @@ namespace CalisthenicsStore.ViewModels.Order
 {
     public class CheckoutViewModel
     {
-        public IEnumerable<CartItemViewModel> CartItems { get; set; }
+        public IEnumerable<CartItemViewModel> CartItems { get; set; } = new List<CartItemViewModel>();
 
         public string CustomerName { get; set; } = null!;
 
