fa0f4cb456307f186438c299657a683ac77a9a43
Ivan Ganchev <djonatan2002@gmail.com>
2025-06-30 07:21:25 +0300

    Created ExerciseViewModel for Index View

--- CalisthenicsStore.ViewModels/CartItem/CartItemViewModel.cs
+++ CalisthenicsStore.ViewModels/CartItem/CartItemViewModel.cs
@@ -3,9 +3,9 @@
     public class CartItemViewModel
     {
         public int ProductId { get; set; }
-        public string ProductName { get; set; }
+        public string ProductName { get; set; } = null!;
 
-        public string ImageUrl { get; set; }   
+        public string? ImageUrl { get; set; }   
 
         public int Quantity { get; set; }
 
---
+++ CalisthenicsStore.ViewModels/Exercise/ExerciseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CalisthenicsStore.Common.Enums;
+
+namespace CalisthenicsStore.ViewModels.Exercise
+{
+    public class ExerciseViewModel
+    {
+        [Required]
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public DifficultyLevel LevelEnum { get; set; }
+
+        [Required]
+        public string Level { get; set; } = null!;
+    }
+}
