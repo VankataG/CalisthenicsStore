18623413f5c3e7e9194fed10533e059bb55f7cc2
Ivan Ganchev <djonatan2002@gmail.com>
2025-06-30 07:45:24 +0300

    Seed Exercises for Test, Applied Migration

--- CalisthenicsStore.Data/Configuration/ExerciseConfiguration.cs
+++ CalisthenicsStore.Data/Configuration/ExerciseConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using CalisthenicsStore.Data.Models;
+﻿using CalisthenicsStore.Common.Enums;
+using CalisthenicsStore.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -24,8 +25,58 @@ namespace CalisthenicsStore.Data.Configuration
                 .Property(e => e.ImageUrl)
                 .HasMaxLength(500);
 
+            entity
+                .HasData(this.SeedExercises());
         }
 
+        private List<Exercise> SeedExercises()
+        {
+            List<Exercise> exercises = new List<Exercise>()
+            {
+                new Exercise()
+                {
+                    Id = 1,
+                    Name = "Push-up",
+                    Description = "A push-up is a common calisthenics exercise that involves lowering the body by bending the arms and then pushing back up to the starting position, primarily working the chest, shoulders, and triceps. It's a full-body exercise that can be modified to suit different fitness levels.",
+                    ImageUrl = "https://static.wikia.nocookie.net/parkour/images/e/e0/Push_Up.jpg/revision/latest/scale-to-width-down/483?cb=20141122161108",
+                    Level = DifficultyLevel.Beginner
+                },
+                new Exercise()
+                {
+                    Id = 2,
+                    Name = "Pull-up",
+                    Description = "Pull-up (exercise): This is a strength training exercise where you lift your body weight by pulling yourself upwards against gravity, typically using a bar. It primarily works the muscles in your back, arms, and core.",
+                    ImageUrl = "https://mikereinold.com/wp-content/uploads/rookie-mistakes-the-pullup-main.jpg",
+                    Level = DifficultyLevel.Beginner
+                },
+                new Exercise()
+                {
+                    Id = 3,
+                    Name = "Muscle-up",
+                    Description = "A muscle-up is an advanced calisthenics exercise that combines a pull-up and a dip, requiring both pulling and pushing movements of the upper body. It involves transitioning from a hanging position below a bar or rings to a position above, with arms extended. Muscle-ups are known for building upper body strength and are considered a critical survival skill by some.",
+                    ImageUrl = "https://i0.wp.com/workoutlabs.com/wp-content/uploads/watermarked/Muscle_Up.png?w=1360",
+                    Level = DifficultyLevel.Intermediate
+                },
+                new Exercise()
+                {
+                    Id = 4,
+                    Name = "Front Lever",
+                    Description = "The front lever is a challenging calisthenics exercise where the body is held in a horizontal, straight-arm position, parallel to the ground, with the front of the body facing upwards. It requires immense strength, particularly in the core, back, and shoulders, as well as full-body tension and control.",
+                    ImageUrl = "https://calisthenics.com/wp-content/uploads/2025/01/full-front-lever.jpg",
+                    Level = DifficultyLevel.Advanced
+                },
+                new Exercise()
+                {
+                    Id = 5,
+                    Name = "Planche",
+                    Description = "The planche is an advanced bodyweight exercise, primarily used in gymnastics and calisthenics, where the body is held parallel to the ground, supported only by straight arms. It requires significant strength, particularly in the shoulders, biceps, and core, as well as excellent balance. The planche is an isometric hold, meaning the body remains stationary while under tension.",
+                    ImageUrl = "https://static.wikia.nocookie.net/street-workout/images/5/56/Leeplanche.jpg/revision/latest?cb=20221209193627",
+                    Level = DifficultyLevel.Insane
+                },
 
+            };
+
+            return exercises;
+        }
     }
 }
---
+++ CalisthenicsStore.Data/Migrations/20250630044407_SeedExercisesTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CalisthenicsStore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedExercisesTest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Exercises",
+                columns: new[] { "Id", "Description", "ImageUrl", "Level", "Name" },
+                values: new object[,]
+                {
+                    { 1, "A push-up is a common calisthenics exercise that involves lowering the body by bending the arms and then pushing back up to the starting position, primarily working the chest, shoulders, and triceps. It's a full-body exercise that can be modified to suit different fitness levels.", "https://static.wikia.nocookie.net/parkour/images/e/e0/Push_Up.jpg/revision/latest/scale-to-width-down/483?cb=20141122161108", 0, "Push-up" },
+                    { 2, "Pull-up (exercise): This is a strength training exercise where you lift your body weight by pulling yourself upwards against gravity, typically using a bar. It primarily works the muscles in your back, arms, and core.", "https://mikereinold.com/wp-content/uploads/rookie-mistakes-the-pullup-main.jpg", 0, "Pull-up" },
+                    { 3, "A muscle-up is an advanced calisthenics exercise that combines a pull-up and a dip, requiring both pulling and pushing movements of the upper body. It involves transitioning from a hanging position below a bar or rings to a position above, with arms extended. Muscle-ups are known for building upper body strength and are considered a critical survival skill by some.", "https://i0.wp.com/workoutlabs.com/wp-content/uploads/watermarked/Muscle_Up.png?w=1360", 1, "Muscle-up" },
+                    { 4, "The front lever is a challenging calisthenics exercise where the body is held in a horizontal, straight-arm position, parallel to the ground, with the front of the body facing upwards. It requires immense strength, particularly in the core, back, and shoulders, as well as full-body tension and control.", "https://calisthenics.com/wp-content/uploads/2025/01/full-front-lever.jpg", 2, "Front Lever" },
+                    { 5, "The planche is an advanced bodyweight exercise, primarily used in gymnastics and calisthenics, where the body is held parallel to the ground, supported only by straight arms. It requires significant strength, particularly in the shoulders, biceps, and core, as well as excellent balance. The planche is an isometric hold, meaning the body remains stationary while under tension.", "https://static.wikia.nocookie.net/street-workout/images/5/56/Leeplanche.jpg/revision/latest?cb=20221209193627", 3, "Planche" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Exercises",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
