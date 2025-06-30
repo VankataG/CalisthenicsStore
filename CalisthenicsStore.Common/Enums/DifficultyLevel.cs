6f49eb3a142d92d7e4b7a477266da1d7430c6889
Ivan Ganchev <djonatan2002@gmail.com>
2025-06-30 06:47:31 +0300

    Created enum for the Exercise Model

---
+++ CalisthenicsStore.Common/Enums/DifficultyLevel.cs
@@ -0,0 +1,10 @@
+﻿namespace CalisthenicsStore.Common.Enums
+{
+    public enum DifficultyLevel
+    {
+        Beginner = 0,
+        Intermediate = 1,
+        Advanced = 2,
+        Insane = 3
+    }
+}
