d7b9fc1cbe8fbd97a4a10614ab0a4f3668d5b2ce
Ivan Ganchev <djonatan2002@gmail.com>
2025-07-21 20:11:04 +0300

    Add ErrorMessages for ExerciseValidation

---
+++ CalisthenicsStore.Common/ErrorMessages/Exercise.cs
@@ -0,0 +1,21 @@
+﻿
+using static CalisthenicsStore.Common.Constants.Exercise;
+
+
+namespace CalisthenicsStore.Common.ErrorMessages
+{
+    public static class Exercise
+    {
+        public const string NameRequiredError = "Exercise name is required.";
+
+        public const string NameMaxLengthError = "Name is too long.";
+
+        public const string DescriptionMaxLengthError = "Description is too long.";
+
+        public const string ImageUrlInvalidError = "Please enter a valid URL.";
+
+        public const string ImageUrlMaxLengthError = "IRL is too long.";
+
+        public const string LevelRequiredError = "Please select a difficulty level.";
+    }
+}
