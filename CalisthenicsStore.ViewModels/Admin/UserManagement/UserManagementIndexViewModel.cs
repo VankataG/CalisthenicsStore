421696b29f0dac6268566c8c02850297547435d1
Ivan Ganchev <djonatan2002@gmail.com>
2025-07-23 23:23:07 +0300

    Created UserManagement Index View

---
+++ CalisthenicsStore.ViewModels/Admin/UserManagement/UserManagementIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CalisthenicsStore.ViewModels.Admin.UserManagement
+{
+    public class UserManagementIndexViewModel
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+
+    }
+}
---
+++ CalisthenicsStore.Web/Areas/Admin/Controllers/UserManagement.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using CalisthenicsStore.Data.Models;
+using CalisthenicsStore.ViewModels.Admin.UserManagement;
+using static CalisthenicsStore.Common.RolesConstants;
+
+namespace CalisthenicsStore.Web.Areas.Admin.Controllers
+{
+    public class UserManagement : BaseAdminController
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UserManagement(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<ApplicationUser> users = await userManager.Users.ToListAsync();
+
+            var model = users.Select(u => new UserManagementIndexViewModel()
+            {
+                Id = u.Id.ToString(),
+                FullName = $"{u.FirstName} {u.LastName}",
+                Email = u.Email,
+                Role = userManager.GetRolesAsync(u).Result.FirstOrDefault() ?? UserRoleName
+            });
+
+            return View(model);
+        }
+    }
+}
