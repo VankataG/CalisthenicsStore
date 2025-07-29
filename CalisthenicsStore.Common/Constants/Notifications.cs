022ed2a6d67302c9b69e1967c250cc5ff9077c68
Ivan Ganchev <djonatan2002@gmail.com>
2025-07-29 08:29:35 +0300

    Added Notification Partial and Constants

---
+++ CalisthenicsStore.Common/Constants/Notifications.cs
@@ -0,0 +1,14 @@
+﻿namespace CalisthenicsStore.Common.Constants
+{
+    public static class Notifications
+    {
+
+        public const string InfoMessageKey = "info";
+
+        public const string WarningMessageKey = "warning";
+
+        public const string ErrorMessageKey = "error";
+
+        public const string SuccessMessageKey = "success";
+    }
+}
