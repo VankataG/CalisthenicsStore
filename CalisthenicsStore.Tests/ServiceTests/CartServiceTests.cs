1b2b0756ec55db62c9ebd9b924dce0462fdb61b8
Ivan Ganchev <djonatan2002@gmail.com>
2025-08-15 07:55:51 +0300

    Add CartServiceTests and TestSession

---
+++ CalisthenicsStore.Tests/ServiceTests/CartServiceTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Text.Json;
+using CalisthenicsStore.Data.Models;
+using CalisthenicsStore.Data.Repositories;
+using CalisthenicsStore.Data.Repositories.Interfaces;
+using CalisthenicsStore.Services;
+using CalisthenicsStore.Services.Interfaces;
+using CalisthenicsStore.Tests.ServiceTests.Other;
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollector.InProcDataCollector;
+using Moq;
+using NUnit.Framework;
+
+namespace CalisthenicsStore.Tests.ServiceTests
+{
+    [TestFixture]
+    public class CartServiceTests
+    {
+        private ISession fakeSession;
+
+        private Mock<IHttpContextAccessor> httpContextAccessorMock;
+
+        private Mock<IProductRepository> productRepositoryMock;
+
+        private ICartService cartService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            DefaultHttpContext context = new DefaultHttpContext();
+            fakeSession = new TestSession();
+            context.Session = fakeSession;
+            this.httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            this.httpContextAccessorMock
+                .Setup(ca => ca.HttpContext)
+                .Returns(context);
+
+            this.productRepositoryMock = new Mock<IProductRepository>();
+            this.cartService = new CartService(this.productRepositoryMock.Object, httpContextAccessorMock.Object);
+        }
+
+        [Test]
+        public void PassAlways()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void GetCartShouldReturnEmptyListWithNoCartInSession()
+        {
+            List<CartItem> cart = cartService.GetCart();
+
+            Assert.That(cart, Is.Not.Null);
+            Assert.That(cart, Is.Empty);
+        }
+
+        [Test]
+        public void GetCartShouldReturnCartItems()
+        {
+            List<CartItem> cartItems = new List<CartItem>()
+            {
+                new CartItem()
+                {
+                    ProductId = Guid.NewGuid(),
+                    Quantity = 5
+                },
+                new CartItem()
+                {
+                    ProductId = Guid.NewGuid(),
+                    Quantity = 10
+                }
+            };
+
+            string cartJson = JsonSerializer.Serialize(cartItems);
+            fakeSession.SetString("Cart", cartJson);
+
+            List<CartItem> result = cartService.GetCart();
+
+            Assert.That(result.Count, Is.EqualTo(cartItems.Count));
+            Assert.That(result[0].Quantity, Is.EqualTo(5));
+            Assert.That(result[1].Quantity, Is.EqualTo(10));
+        }
+
+    }
+}
---
+++ CalisthenicsStore.Tests/ServiceTests/Other/TestSession.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using NUnit.Framework.Constraints;
+
+namespace CalisthenicsStore.Tests.ServiceTests.Other
+{
+    public class TestSession : ISession
+    {
+        private readonly Dictionary<string, byte[]> sessionStorage = new Dictionary<string, byte[]>();
+
+        public bool IsAvailable => true;
+
+        public string Id { get; } = Guid.NewGuid().ToString();
+
+        public IEnumerable<string> Keys => sessionStorage.Keys;
+
+        public Task LoadAsync(CancellationToken cancellationToken = default) => Task.CompletedTask;
+
+        public Task CommitAsync(CancellationToken cancellationToken = default) => Task.CompletedTask;
+        
+        public bool TryGetValue(string key, [NotNullWhen(true)] out byte[]? value) => sessionStorage.TryGetValue(key, out value);
+
+        public void Set(string key, byte[] value) => sessionStorage[key] = value;
+
+        public void Remove(string key) => sessionStorage.Remove(key);
+
+        public void Clear() => sessionStorage.Clear();
+
+    }
+}
