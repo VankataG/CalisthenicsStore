a32d7d06e7fcacbf3c5186863ad72096df8a3ec2
Ivan Ganchev <djonatan2002@gmail.com>
2025-09-04 19:55:55 +0300

    Create ready for tests class for ProductManagementService

---
+++ CalisthenicsStore.Tests/ServiceTests/ProductManagementServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using CalisthenicsStore.Data.Repositories.Interfaces;
+using CalisthenicsStore.Services.Admin;
+using CalisthenicsStore.Services.Admin.Interfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace CalisthenicsStore.Tests.ServiceTests
+{
+
+    [TestFixture]
+    public class ProductManagementServiceTests
+    {
+        private IMock<IProductRepository> productRepositoryMock;
+        private IMock<ICategoryRepository> categoryRepositoryMock;
+
+        private IProductManagementService productService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.productRepositoryMock = new Mock<IProductRepository>();
+            this.categoryRepositoryMock = new Mock<ICategoryRepository>();
+            this.productService =
+                new ProductManagementService(productRepositoryMock.Object, categoryRepositoryMock.Object);
+        }
+
+        [Test]
+        public void PassAlways()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
