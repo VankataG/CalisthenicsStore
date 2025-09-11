0e42aa23d97dce5def5114e8d38ed0ae2cff89cf
Ivan Ganchev <djonatan2002@gmail.com>
2025-09-11 21:47:54 +0300

    Create ProfileViewModel

---
+++ CalisthenicsStore.ViewModels/ApplicationUser/ProfileViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace CalisthenicsStore.ViewModels.ApplicationUser
+{
+    public class ProfileViewModel
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public bool EmailConfirmed { get; set; }
+        public string? PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public bool TwoFactorEnabled { get; set; }
+        public bool LockoutEnabled { get; set; }
+        public DateTimeOffset? LockoutEnd { get; set; }
+        public int AccessFailedCount { get; set; }
+        public IEnumerable<string> Roles { get; set; } = Enumerable.Empty<string>();
+    }
+}
