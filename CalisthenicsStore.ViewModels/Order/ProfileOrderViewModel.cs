1abcbd20bc36451a144d2b26a04fe7b93861f8aa
Ivan Ganchev <djonatan2002@gmail.com>
2025-09-13 15:38:33 +0300

    Created viewmodels for Product and Order for viewing in Profile section

---
+++ CalisthenicsStore.ViewModels/Order/ProfileOrderViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CalisthenicsStore.Data.Models;
+using CalisthenicsStore.ViewModels.Product;
+
+namespace CalisthenicsStore.ViewModels.Order
+{
+    public class ProfileOrderViewModel
+    {
+        public Guid Id { get; set; }
+
+        public Guid ApplicationUserId { get; set; }
+
+        public string City { get; set; } = null!;
+
+        public string Address { get; set; } = null!;
+
+        public DateTime OrderDate { get; set; }
+
+        public string Status { get; set; } = null!;
+
+        public IEnumerable<ProfileProductViewModel> Products { get; set; }
+            = Enumerable.Empty<ProfileProductViewModel>();
+
+    }
+}
---
+++ CalisthenicsStore.ViewModels/Product/ProfileProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace CalisthenicsStore.ViewModels.Product
+{
+    public class ProfileProductViewModel
+    {
+        public Guid ProductId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string ImageUrl { get; set; } = null!;
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal Total { get; set; }
+    }
+}
