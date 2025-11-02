82b63ca6b1fc952a49b016c46df7b58c119fad92
Ivan Ganchev <djonatan2002@gmail.com>
2025-11-02 20:53:26 +0200

    Created ReCaptchaService and Interface

---
+++ CalisthenicsStore.Services/Interfaces/IReCaptchaServ.cs
@@ -0,0 +1,7 @@
+﻿namespace CalisthenicsStore.Services.Interfaces
+{
+    public interface IReCaptchaServ
+    {
+        Task<bool> VerifyAsync(string? token, CancellationToken ct = default);
+    }
+}
---
+++ CalisthenicsStore.Services/ReCaptchaServ.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http.Json;
+using CalisthenicsStore.Data.Models.ReCaptcha;
+using CalisthenicsStore.Services.Interfaces;
+using Microsoft.Extensions.Options;
+
+namespace CalisthenicsStore.Services
+{
+    public class ReCaptchaServ : IReCaptchaServ
+    {
+        private readonly HttpClient http;
+
+        private readonly GoogleReCaptchaSettings captchaSett;
+
+        public ReCaptchaServ(HttpClient http, IOptions<GoogleReCaptchaSettings> captchaSett)
+        {
+            this.http = http;
+            this.captchaSett = captchaSett.Value;
+        }
+
+        private sealed class ReCaptchaResponse
+        {
+            public bool Success { get; set; }
+            public List<string> ErrorCodes { get; set; } = new();
+        }
+
+        public async Task<bool> VerifyAsync(string? token, CancellationToken ct = default)
+        {
+            if (string.IsNullOrWhiteSpace(token)) return false;
+
+            var url = $"https://www.google.com/recaptcha/api/siteverify?secret={captchaSett.SecretKey}&response={token}";
+            using var res = await http.PostAsync(url, content: null, ct);
+            if (!res.IsSuccessStatusCode) return false;
+
+            var body = await res.Content.ReadFromJsonAsync<ReCaptchaResponse>(cancellationToken: ct);
+            return body?.Success == true;
+        }
+    }
+}
