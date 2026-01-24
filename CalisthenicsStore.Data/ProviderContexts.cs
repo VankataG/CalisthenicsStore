1540fd2f052ce87548fc80fee9bdff7a78d3a74f
Ivan Ganchev <djonatan2002@gmail.com>
2026-01-24 17:28:47 +0200

    Add 2 contexts for SQL and Postgres

---
+++ CalisthenicsStore.Data/ProviderContexts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CalisthenicsStore.Data
+{
+    public class SqlServerCalisthenicsStoreDbContext : CalisthenicsStoreDbContext
+    {
+        public SqlServerCalisthenicsStoreDbContext(DbContextOptions<SqlServerCalisthenicsStoreDbContext> options)
+            : base(options)
+        {
+        }
+    }
+
+    public class PostgresCalisthenicsStoreDbContext : CalisthenicsStoreDbContext
+    {
+        public PostgresCalisthenicsStoreDbContext(DbContextOptions<PostgresCalisthenicsStoreDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
