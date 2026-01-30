b67298462f4f11f3fdb8f12296a5aeef4be8175c
Ivan Ganchev <djonatan2002@gmail.com>
2026-01-30 08:42:12 +0200

    Add proper service to control supabase with interface

---
+++ CalisthenicsStore.Services/Admin/Interfaces/IFileStorageService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CalisthenicsStore.Services.Admin.Interfaces
+{
+    public interface IFileStorageService
+    {
+        Task<string?> UploadImageAsync(IFormFile file);
+    }
+}
---
+++ CalisthenicsStore.Services/Admin/SupabaseStorageService.cs
@@ -0,0 +1,39 @@
+﻿using CalisthenicsStore.Services.Admin.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace CalisthenicsStore.Services.Admin
+{
+    public class SupabaseStorageService : IFileStorageService
+    {
+        private readonly Supabase.Client client;
+
+        private const string BucketName = "product-images";
+
+
+            
+        public SupabaseStorageService(Supabase.Client client)
+        {
+            this.client = client;
+        }
+
+        public async Task<string?> UploadImageAsync(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return null;
+
+            var ext = Path.GetExtension(file.Name);
+            var fileName = $"{Guid.NewGuid}{ext}";
+
+            using MemoryStream ms = new MemoryStream();
+            await file.CopyToAsync(ms);
+
+            await client.Storage
+                .From(BucketName)
+                .Upload(ms.ToArray(), fileName);
+
+            return client.Storage
+                .From(BucketName)
+                .GetPublicUrl(fileName);
+        }
+    }
+}
