5a29fbf0252f1b92661b1596a88eaf23e1988d30
Ivan Ganchev <djonatan2002@gmail.com>
2026-02-02 11:27:17 +0200

    Add new VM for editing product

---
+++ CalisthenicsStore.ViewModels/Admin/ProductManagement/ProductEditModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+using static CalisthenicsStore.Common.Constants.Product;
+using static CalisthenicsStore.Common.ErrorMessages.Product;
+
+
+namespace CalisthenicsStore.ViewModels.Admin.ProductManagement
+{
+    public class ProductEditModel
+    {
+        public Guid? Id { get; set; }
+
+        [Required(ErrorMessage = NameRequiredError)]
+        [MaxLength(NameMaxLength, ErrorMessage = NameMaxLengthError)]
+        public string Name { get; set; } = null!;
+
+        [MaxLength(DescriptionMaxLength, ErrorMessage = DescriptionMaxLengthError)]
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public string ExistingImageUrl { get; set; } = "/images/no-image.jpg";
+
+        public IFormFile? NewImageFile { get; set; }
+
+        public Guid CategoryId { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; } = new List<SelectListItem>();
+    }
+}
