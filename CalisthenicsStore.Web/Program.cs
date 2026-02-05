e9cee137b89aa50f847cb35998df00474a41c490
Ivan Ganchev <djonatan2002@gmail.com>
2026-02-05 07:29:56 +0200

    Big refactor cleanup

--- CalisthenicsStore.Web/Program.cs
+++ CalisthenicsStore.Web/Program.cs
@@ -1,15 +1,10 @@
-using CalisthenicsStore.Data;
-using CalisthenicsStore.Data.Models.ReCaptcha;
 using CalisthenicsStore.Data.Repositories.Interfaces;
 using CalisthenicsStore.Data.Seeding;
 using CalisthenicsStore.Data.Seeding.Interfaces;
 using CalisthenicsStore.Data.Utilities;
 using CalisthenicsStore.Data.Utilities.Interfaces;
-using CalisthenicsStore.Services;
 using CalisthenicsStore.Services.Interfaces;
 using CalisthenicsStore.Web.Extensions;
-using CalisthenicsStore.Web.Models;
-using Microsoft.EntityFrameworkCore;
 using Stripe;
 
 
@@ -17,22 +12,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDatabaseAndIdentity(builder.Configuration, builder.Environment);
 
-//Add Supabase storage
-builder.Services.AddSingleton(provider =>
-{
-    var config = provider.GetRequiredService<IConfiguration>();
-
-    var url = config["Supabase:Url"];
-    var key = config["Supabase:ServiceRoleKey"];
-    var options = new Supabase.SupabaseOptions
-    {
-        AutoConnectRealtime = false,
-    };
-
-    return new Supabase.Client(url!, key, options);
-});
-
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddRazorPages();
@@ -42,27 +21,12 @@ builder.Services.AddUserDefinedServices(typeof(IProductService).Assembly);
 builder.Services.AddRepositories(typeof(IProductRepository).Assembly);
 builder.Services.AddTransient<IIdentitySeeder, IdentitySeeder>();
 
-
-//Adding sessions for the Cart
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(60);
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
-
-//ReCaptcha
-builder.Services.Configure<GoogleReCaptchaSettings>(builder.Configuration.GetSection("GoogleReCaptcha"));
-builder.Services.AddHttpClient<IReCaptchaServ, ReCaptchaServ>();
-
-//Configure StripeSettings using values from appsettings.json
-builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
-
+builder.Services.AddSupabase(builder.Configuration);
+builder.Services.AddReCaptcha(builder.Configuration);
+builder.Services.AddStripe(builder.Configuration);
+builder.Services.AddCartSession();
 
 var app = builder.Build();
-
-await app.ApplyMigrationsAndSeedDataAsync();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -72,7 +36,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -85,6 +48,7 @@ app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseSession();
 
 app.SeedDefaultIdentity();
 
@@ -92,17 +56,21 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.UseAdminRedirection();
-app.UseSession(); //Here we use the session needed for the Cart
 
 app.MapControllerRoute(
     name: "areas",
     pattern: "{area}/{controller=Home}/{action=Index}/{id?}");
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages();
 
 //Configure Stripe secret key from configuration
 StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe")["SecretKey"];
+
+//Startup work
+await app.ApplyMigrationsAndSeedDataAsync();
 
 app.Run();
