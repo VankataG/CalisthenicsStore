2033986489c5159d95b86d6619d8e75f9cfdca9e
VankataG <ivg.ganchev@gmail.com>
2026-02-22 20:07:59 +0200

    Add DTO for seeding products

---
+++ CalisthenicsStore.Data/Utilities/DTOs/SeedProductDto.cs
@@ -0,0 +1,17 @@
+﻿namespace CalisthenicsStore.Data.Utilities.DTOs
+{
+    public sealed class SeedProductDto
+    {
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        public Guid CategoryId { get; set; }
+
+        public int StockQuantity { get; set; }
+    }
+}
