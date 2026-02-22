75978ae63b8ead9b7a0c836c75d706aa73c9b184
VankataG <ivg.ganchev@gmail.com>
2026-02-22 20:08:59 +0200

    Refactor Import method

--- CalisthenicsStore.Data/Utilities/DataProcessor.cs
+++ CalisthenicsStore.Data/Utilities/DataProcessor.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 using CalisthenicsStore.Data.Models;
 using CalisthenicsStore.Data.Utilities.Interfaces;
+using CalisthenicsStore.Data.Utilities.DTOs;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace CalisthenicsStore.Data.Utilities
 {
@@ -15,28 +17,45 @@ namespace CalisthenicsStore.Data.Utilities
             this.entityValidator = entityValidator;
         }
 
-        public async Task ImportProductsFromJson(CalisthenicsStoreDbContext dbContext)
+        public async Task ImportProductsFromJson(CalisthenicsStoreDbContext dbContext, string supabaseUrl, string bucket)
         {
             if (await dbContext.Products.AnyAsync()) return;
 
             string path = Path.Combine(AppContext.BaseDirectory, "Files", "products.json");
             Console.WriteLine("[SEED] Looking for: " + path);
 
-
             if (!File.Exists(path))
-            {
                 throw new FileNotFoundException($"Seed file not found: {path}");
-            }
+            
+            string productsData = await File.ReadAllTextAsync(path);
 
-            string productsStr = await File.ReadAllTextAsync(path);
+            var productsDtos = JsonSerializer.Deserialize<List<SeedProductDto>>(productsData);
 
-            var products = JsonSerializer.Deserialize<List<Product>>(productsStr);
+            if (productsDtos == null || productsDtos.Count == 0) return;
 
-            if (products != null && products.Count > 0)
+
+            supabaseUrl = supabaseUrl.TrimEnd('/');
+            bucket = bucket.Trim('/');
+
+            string basePublicUrl = $"{supabaseUrl}/storage/v1/object/public/{bucket}/";
+
+
+            List<Product> products = new List<Product>();
+            foreach (var productDto in productsDtos)
             {
-                    await dbContext.Products.AddRangeAsync(products);
-                    await dbContext.SaveChangesAsync();
+                products.Add(new Product
+                {
+                    Name = productDto.Name,
+                    Description = productDto.Description,
+                    Price = productDto.Price,
+                    ImageUrl = string.IsNullOrWhiteSpace(productDto.ImagePath) ? "/images/no-image.jpg" : basePublicUrl + productDto.ImagePath.TrimStart('/'),
+                    CategoryId = productDto.CategoryId,
+                    StockQuantity = productDto.StockQuantity
+                });
             }
+
+            await dbContext.Products.AddRangeAsync(products);
+            await dbContext.SaveChangesAsync();
         }
 
     }
